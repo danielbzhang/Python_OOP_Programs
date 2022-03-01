8cede5c447c46ec70c970422fb8f8d5d996fac14
danielbzhang <daniel.b.zhangusa@gmail.com>
2022-03-01 14:12:08 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import turtle
+
+myTurtle = turtle.Turtle()   # create an instance of the Turtle class
+
+myTurtle.penup()
+myTurtle.goto(50, 75)
+myTurtle.pendown()
+
+myTurtle.forward(100)
+myTurtle.left(90)
+myTurtle.forward(200)
+myTurtle.left(90)
+myTurtle.forward(100)
+myTurtle.left(90)
+myTurtle.forward(200)
+
+
+
+turtle.exitonclick()
